54b513ba5a5b22850aa949aae35772241105cce0
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2021-10-20 22:16:23 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,51 +8,6 @@ $('.start').click(function(){
 })
 
 
-window.onload = function() {
-    // seperate time span & lyric
-    var timeSpan = [];
-    var count1 = 1;
-    while(count1 < splitText.length) {
-        console.log('1');
-        timeSpan.push(splitText[count1]);
-        count1 += 4;
-    }
-
-    // start time
-    var startTime = [];
-    var count3 = 0;
-    while(count3 < timeSpan.length) {
-        console.log('2');
-        var sp = timeSpan[count3].split(' --> ')[0];
-        startTime.push(sp);
-        count3 += 1;
-    }
-
-
-    // end time
-    var endTime = [];
-    var count4 = 0
-    while(count4 < timeSpan.length) {
-        console.log('3');
-        var sp = timeSpan[count4].split(' --> ')[1];
-        endTime.push(sp);
-
-        if(count4 + 1 == timeSpan.length) var terminatePlay = sp;
-        count4 += 1;
-    }
-
-
-    // seperate lyrics
-    var lyricText = [];
-    var count2 = 2;
-    while(count2 < splitText.length) {
-        console.log('4');
-        lyricText.push(splitText[count2]);
-        count2 += 4;
-    }
-}
-
-
 // read srt file
 var srtFile = $.ajax({url: "assets/src/world.execute(me)-en.srt", async: false});
 var fullSrt = srtFile.responseText;
@@ -65,6 +20,49 @@ var fullCode = codeFile.responseText;
 var splitCode = fullCode.split('\r\n\r\n\r\n\r\n\r\n\r\n');
 for(var i = 0; i < splitCode.length; i++) {
     splitCode[i] = splitCode[i].replace(/\r\n/g, '<br/>');
+}
+
+
+// seperate time span & lyric
+var timeSpan = [];
+var count1 = 1;
+while(count1 < splitText.length) {
+    console.log('1');
+    timeSpan.push(splitText[count1]);
+    count1 += 4;
+}
+
+// start time
+var startTime = [];
+var count3 = 0;
+while(count3 < timeSpan.length) {
+    console.log('2');
+    var sp = timeSpan[count3].split(' --> ')[0];
+    startTime.push(sp);
+    count3 += 1;
+}
+
+
+// end time
+var endTime = [];
+var count4 = 0
+while(count4 < timeSpan.length) {
+    console.log('3');
+    var sp = timeSpan[count4].split(' --> ')[1];
+    endTime.push(sp);
+
+    if(count4 + 1 == timeSpan.length) var terminatePlay = sp;
+    count4 += 1;
+}
+
+
+// seperate lyrics
+var lyricText = [];
+var count2 = 2;
+while(count2 < splitText.length) {
+    console.log('4');
+    lyricText.push(splitText[count2]);
+    count2 += 4;
 }
 
 
